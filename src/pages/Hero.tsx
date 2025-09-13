cb5539c7aa0f0fb77af0d8af7913fa12f8cb78b8
Yusufnova06 <yusufnovandra377@gmail.com>
2025-09-13 13:40:24 +0700

    refactor: wave color

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -78,19 +78,19 @@ export default function Hero() {
         
           <div className="absolute bottom-0 w-[200%] h-20 animate-waveMedium opacity-80 flex">
             <svg className="w-1/2 h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M0,49 C150,89 350,9 600,49 C850,89 1050,9 1200,49 L1200,120 L0,120 Z" className="fill-[#1a1a1a]" />
+              <path d="M0,49 C150,89 350,9 600,49 C850,89 1050,9 1200,49 L1200,120 L0,120 Z" className="fill-[#afafaf]/30" />
             </svg>
             <svg className="w-1/2 h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M0,49 C150,89 350,9 600,49 C850,89 1050,9 1200,49 L1200,120 L0,120 Z" className="fill-[#1a1a1a]" />
+              <path d="M0,49 C150,89 350,9 600,49 C850,89 1050,9 1200,49 L1200,120 L0,120 Z" className="fill-[#afafaf]/30" />
             </svg>
           </div>
 
           <div className="absolute bottom-0 w-[200%] h-20 animate-waveSlow opacity-60 flex">
             <svg className="w-1/2 h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M0,49 C150,89 350,9 600,49 C850,89 1050,9 1200,49 L1200,120 L0,120 Z" className="fill-[#2a2a2a]" />
+              <path d="M0,49 C150,89 350,9 600,49 C850,89 1050,9 1200,49 L1200,120 L0,120 Z" className="fill-[#d8b0b0]/30" />
             </svg>
             <svg className="w-1/2 h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M0,49 C150,89 350,9 600,49 C850,89 1050,9 1200,49 L1200,120 L0,120 Z" className="fill-[#2a2a2a]" />
+              <path d="M0,49 C150,89 350,9 600,49 C850,89 1050,9 1200,49 L1200,120 L0,120 Z" className="fill-[#d8b0b0]/30" />
             </svg>
           </div>
 
