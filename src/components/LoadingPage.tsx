acbb5603649d6164490e42ae9487c2a546e2d6eb
Yusufnova06 <yusufnovandra377@gmail.com>
2025-09-25 15:30:11 +0700

    feat: enhance LoadingPage with desktop usage prompt

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import loading from "../assets/backgrounds/loading.webp";
+import { ChevronsLeftRightEllipsis, Monitor } from "lucide-react";
 
 interface LoadingPageProps {
   onFinish: (status: string) => void;
@@ -27,13 +28,13 @@ export default function LoadingPage({ onFinish }: LoadingPageProps) {
   }, [stage, onFinish]);
 
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-[#090909] relative">
+    <div className="flex items-center justify-center flex-col gap-3 w-screen h-screen bg-[#090909] relative">
       {stage === "start" && (
         <div
           className="text-center text-white cursor-pointer"
           onClick={() => setStage("loading")}
         >
-          <p className="text-lg font-minecraft2 animate-pulse">Click to Start</p>
+          <p className="text-lg font-minecraft2 animate-pulse" style={{ textShadow: "2px 2px 4px rgba(255, 255, 255, 1)" }}>Click to Start</p>
         </div>
       )}
 
@@ -44,9 +45,8 @@ export default function LoadingPage({ onFinish }: LoadingPageProps) {
           <img
             src={loading}
             alt="Loading..."
-            className={`relative z-10 w-40 h-40 transition-opacity duration-700 ease-in-out ${
-              fadeOut ? "opacity-0" : "opacity-100"
-            }`}
+            className={`relative z-10 w-40 h-40 transition-opacity duration-700 ease-in-out ${fadeOut ? "opacity-0" : "opacity-100"
+              }`}
           />
 
           <p className="relative z-10 mt-4 text-white font-minecraft2 text-lg">
@@ -54,6 +54,13 @@ export default function LoadingPage({ onFinish }: LoadingPageProps) {
           </p>
         </div>
       )}
+      <div className="font-minecraft2 text-gray-500 text-sm">
+        for better experience, please use desktop <Monitor className="inline w-4 h-4" />
+      </div>
+      <div className="absolute bottom-10 right-10 flex items-center text-white text-sm font-minecraft2">
+        <ChevronsLeftRightEllipsis className="w-4 h-4 mr-2" />
+        <span>Portfolio v1.0.0</span>
+      </div>
     </div>
   );
 }
